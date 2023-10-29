e23fb9221a77b785da4b321533719221646b3826
Venom <jasicaxaayu@gmail.com>
2023-10-29 23:19:10 +0530

    Update __main__.py

--- VipX/__main__.py
+++ VipX/__main__.py
@@ -24,7 +24,7 @@ async def init():
         and not config.STRING5
     ):
         LOGGER("VipX").error(
-            "🙄 𝐒𝐭𝐫𝐢𝐧𝐠 𝐒𝐞𝐬𝐬𝐢𝐨𝐧 𝐍𝐨𝐭 𝐅𝐢𝐥𝐥𝐞𝐝, 𝐏𝐥𝐞𝐚𝐬𝐞 𝐅𝐢𝐥𝐥 𝐀 𝐏𝐲𝐫𝐨𝐠𝐫𝐚𝐦 𝐒𝐞𝐬𝐬𝐢𝐨𝐧 😐"
+            "sᴛʀɪɴɢ sᴇssɪᴏɴ ᴋᴏɴ ᴛᴇʀᴀ ʙᴀᴀᴘ ᴅᴀʟᴇɢᴀ ʙsᴅᴋ"
         )
         return
     if (
@@ -32,7 +32,7 @@ async def init():
         and not config.SPOTIFY_CLIENT_SECRET
     ):
         LOGGER("VipX").warning(
-            "🥲 𝐒𝐢𝐫 𝐒𝐩𝐨𝐭𝐢𝐟𝐲 𝐈𝐝 & 𝐒𝐞𝐜𝐫𝐞𝐭 𝐍𝐨𝐭 𝐅𝐢𝐥𝐥𝐞𝐝. 𝐃𝐨𝐧𝐭 𝐖𝐨𝐫𝐫𝐲 𝐍𝐨𝐭 𝐏𝐫𝐨𝐛𝐥𝐞𝐦 𝐄𝐧𝐣𝐨𝐲 𝐓𝐞𝐧𝐬𝐢𝐨𝐧 𝐅𝐫𝐞𝐞 🥰"
+            "sᴘᴏᴛɪғʏ ɪᴅ ᴘᴀᴘᴀ ᴀᴀʏᴇɢᴇ ᴅᴀʟɴᴇ ʙsᴅᴋ"
         )
     try:
         users = await get_gbanned()
@@ -47,7 +47,7 @@ async def init():
     for all_module in ALL_MODULES:
         importlib.import_module("VipX.plugins." + all_module)
     LOGGER("VipX.plugins").info(
-        "😋 𝐀𝐥𝐥 𝐅𝐞𝐚𝐭𝐮𝐫𝐞𝐬 𝐋𝐨𝐚𝐝𝐞𝐝 𝐁𝐚𝐛𝐲 🎉"
+        "ᴄʜᴀʟ ʜᴏ ɢʏᴀ sᴀʙ"
     )
     await userbot.start()
     await Vip.start()
